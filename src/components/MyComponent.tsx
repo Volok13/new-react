f5e8e58b4f842b18920410bee6d22a03719393bc
Volok13 <volokhova2017@gmail.com>
2025-01-05 18:58:05 +0200

    hm1

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,9 @@
+import {FC} from "react";
+
+type MyComponentPropType = {text: string};
+
+const MyComponent :FC<MyComponentPropType> = ({text}) => {
+    return <div>{text}</div>
+}
+
+export default MyComponent;
