7f097da886f27ecee52e70aa0b87d8bd7f701df2
Volok13 <volokhova2017@gmail.com>
2025-01-05 18:59:27 +0200

    hm1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
-      hello okten
+      <MyComponent text={'hello1'}/>
     </>
   )
 }
