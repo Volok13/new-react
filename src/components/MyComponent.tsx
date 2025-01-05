95e91ac91517245c21b80a21d6b3b110d8234026
Volok13 <volokhova2017@gmail.com>
2025-01-05 19:50:23 +0200

    hm2

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,10 +1,9 @@
 import {FC} from "react";
-import styles from './MyComponent.module.css'
 
 type MyComponentPropType = {text: string};
 
 const MyComponent :FC<MyComponentPropType> = ({text}) => {
-    return <div className={styles.target}>{text}</div>
+    return <div className='text-3xl font-bold underline'>{text}</div>
 }
 
 export default MyComponent;
