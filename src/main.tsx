90da7d6f0d030e5de3a4b3d0fb5e7a30cb96dd8d
Volok13 <volokhova2017@gmail.com>
2025-01-12 02:06:41 +0200

    hm3

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.tsx";
+import APage from "./pages/APage.tsx";
+import BPage from "./pages/BPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+
+
+const router = createBrowserRouter([
+    {
+        path:'/', element:<MainLayout/>,
+        children:[
+            {path:'', element:<HomePage/>},
+            {path:'a', element:<APage/>},
+            {path:'b', element:<BPage/>, children: [
+    {path:'1', element:<div>1</div>},
+    {path:'2', element:<div>2</div>},
+    {path:'3', element:<div>3</div>}
+]
+}
+        ]
+
+
+    }
+    // {path:'/a', element:<div>layout a</div>},
+    // {path:'/b', element:<div>layout b</div>},
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
