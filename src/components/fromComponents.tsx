1ded4c6c61bb78f8bf76265d18aa6a7fceea2106
Volok13 <volokhova2017@gmail.com>
2025-01-19 13:01:50 +0200

    lesson5

---
+++ src/components/fromComponents.tsx
@@ -0,0 +1,14 @@
+
+
+const FromComponents = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'name'}/>
+                <input type="text" name={'password'}/>
+            </form>
+        </div>
+    );
+};
+
+export default FromComponents;
