37034e29fe5e14079eab6725202b4d0605e2234d
Volok13 <volokhova2017@gmail.com>
2025-01-20 20:19:57 +0200

    lesson5.4

--- src/components/FormsComponents.tsx
+++ src/components/FormsComponents.tsx
@@ -1,4 +1,6 @@
 import {useForm} from "react-hook-form"
+import {joiResolver} from "@hookform/resolvers/joi";
+import userValidator from "../validators/user.validator.tsx";
 
 
 interface IFormProps {
@@ -14,7 +16,7 @@ const FormsComponents = () => {
         register,
         formState: { errors, isValid },
     } = useForm<IFormProps>({
-        mode:'all'
+        mode:'all', resolver:joiResolver(userValidator)
     });
 
     const coustomHandler = (formDataProps:IFormProps) => {
@@ -25,34 +27,35 @@ const FormsComponents = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(coustomHandler)}>
-               <label> <input type="text" {...register('username', {
-                    required: {value: true, message: 'name is required'},
-                    // pattern:{
-                    //     value:/\w+/,
-                    //     message: 'wrong name',
-                    // },
-                    minLength: {value:4, message:'wrong name'}
-
-                })}/>
+               <label>
+                   <input type="text" {...register('username',
+                   // {required: {value: true, message: 'name is required'},
+                   //  // pattern:{
+                   //  //     value:/\w+/,
+                   //  //     message: 'wrong name',
+                   //  // },
+                   //  minLength: {value:4, message:'wrong name'}}
+               )}/>
                    {errors.username && <div>{errors.username.message}</div>}
 
                </label>
-                <label><input type="text" {...register('password',
-                    {
-                        required:true,
-                        minLength: {value: 3, message: 'pass too short'},
-                        maxLength: {value:6, message:'pass too lenght'}
-                    }
+                <label>
+                    <input type="text" {...register('password',
+                    // {
+                    //     required:true,
+                    //     minLength: {value: 3, message: 'pass too short'},
+                    //     maxLength: {value:6, message:'pass too lenght'}
+                    // }
                 )}/>
                     {errors.password && <div>{errors.password.message}</div>}
                 </label>
-                <label><input type="number" {...register('age', {
-
-                    required: true,
-                    valueAsNumber: true,
-                    min:{value:1, message:'age too small'},
-                    max: {value: 117, message:'age too big'},
-                })}/>
+                <label>
+                    <input type="number" {...register('age',
+                    // {required: true,
+                    // valueAsNumber: true,
+                    // min:{value:1, message:'age too small'},
+                    // max: {value: 117, message:'age too big'} }
+                )}/>
                     {errors.age && <div>{errors.age.message}</div>}
                 </label>
                 <button disabled={!isValid}>send</button>
---
+++ src/validators/user.validator.tsx
@@ -0,0 +1,18 @@
+import * as Joi from "joi";
+
+
+const userValidator = Joi.object({
+    username: Joi.string().pattern(/w{4,}/).required().messages({
+        'string.pattern.base':'only chars allowed & this must be at least 4 characters',
+    }),
+    password: Joi.string().min(3).max(6).required().messages({
+        'string.min':'password can be at least 3 chars',
+        'string.max':'password must be gt 6 chars',
+    }),
+    age: Joi.number().min(1).max(117).required().messages({
+        'number.min':'min age is 1',
+        'number.max':'max age is 117',
+    })
+})
+
+export default userValidator;
